9760078db29e932da232abec40228801e45a1a9b
alcidessainz99@gmail.com <alcidessainz99@gmail.com>
2024-12-04 14:56:04 -0500

    borrar genero terminado

--- angular-peliculas/src/app/app.config.ts
+++ angular-peliculas/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +10,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
@@ -17,15 +22,16 @@ export const appConfig: ApplicationConfig = {
     },
     provideMomentDateAdapter({
       parse: {
-        dateInput:['DD-MM-YYYY']
+        dateInput: ['DD-MM-YYYY'],
       },
       display: {
         dateInput: 'DD-MM-YYYY',
         monthYearLabel: 'MMM YYYY',
         dateA11yLabel: 'LL',
-        monthYearA11yLabel:'MMMM YYYY'
-      }
+        monthYearA11yLabel: 'MMMM YYYY',
+      },
     }),
-    provideHttpClient(withFetch())
-  ]
+    provideHttpClient(withFetch()),
+    importProvidersFrom([SweetAlert2Module.forRoot()]),
+  ],
 };
--- angular-peliculas/src/app/generos/indice-generos/indice-generos.component.ts
+++ angular-peliculas/src/app/generos/indice-generos/indice-generos.component.ts
@@ -8,6 +8,7 @@ import { MatTableModule } from '@angular/material/table';
 import { HttpResponse } from '@angular/common/http';
 import { PaginacionDTO } from '../../compartidos/modelos/PaginacionDTO';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @Component({
   selector: 'app-indice-generos',
@@ -18,12 +19,12 @@ import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
     ListadoGenericoComponent,
     MatTableModule,
     MatPaginatorModule,
+    SweetAlert2Module,
   ],
   templateUrl: './indice-generos.component.html',
   styleUrl: './indice-generos.component.css',
 })
 export class IndiceGenerosComponent {
-
   generosService = inject(GeneroService);
   generos!: GeneroDTO[];
   columnasMostrar = ['id', 'nombre', 'acciones'];
@@ -35,14 +36,27 @@ export class IndiceGenerosComponent {
   }
 
   cargarRegistros() {
-    this.generosService.obtenerPaginado(this.paginacion).subscribe((respuesta: HttpResponse<GeneroDTO[]>) => {
+    this.generosService
+      .obtenerPaginado(this.paginacion)
+      .subscribe((respuesta: HttpResponse<GeneroDTO[]>) => {
         this.generos = respuesta.body as GeneroDTO[];
-        const cabecera = respuesta.headers.get("cantidad-total-registros") as string;
+        const cabecera = respuesta.headers.get(
+          'cantidad-total-registros'
+        ) as string;
         this.cantidadRegistros = parseInt(cabecera, 10);
       });
   }
   actualizarPaginacion(datos: PageEvent) {
-    this.paginacion = {pagina: datos.pageIndex + 1,recordsPorPagina: datos.pageSize,};
+    this.paginacion = {
+      pagina: datos.pageIndex + 1,
+      recordsPorPagina: datos.pageSize,
+    };
     this.cargarRegistros();
+  }
+  borrar(id: number) {
+    this.generosService.borrar(id).subscribe(() => {
+      this.paginacion = { pagina: 1, recordsPorPagina: 5 };
+      this.cargarRegistros();
+    });
   }
 }

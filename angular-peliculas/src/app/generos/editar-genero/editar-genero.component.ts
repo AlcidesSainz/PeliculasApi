d90429634714703d06a0f4c585a7c1f3db02f013
alcidessainz99@gmail.com <alcidessainz99@gmail.com>
2024-12-04 14:13:47 -0500

    actualizacion de generos front hecho

--- angular-peliculas/src/app/generos/editar-genero/editar-genero.component.ts
+++ angular-peliculas/src/app/generos/editar-genero/editar-genero.component.ts
@@ -1,21 +1,52 @@
-import { Component, Input, numberAttribute } from '@angular/core';
+import {
+  Component,
+  inject,
+  Inject,
+  Input,
+  numberAttribute,
+  OnInit,
+} from '@angular/core';
 import { FormularioGeneroComponent } from '../formulario-genero/formulario-genero.component';
 import { GeneroCreacionDTO, GeneroDTO } from '../generos';
+import { GeneroService } from '../genero.service';
+import { CargandoComponent } from '../../compartidos/componentes/cargando/cargando.component';
+import { MostrarErroresComponent } from '../../compartidos/componentes/mostrar-errores/mostrar-errores.component';
+import { Router } from '@angular/router';
+import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
 
 @Component({
   selector: 'app-editar-genero',
   standalone: true,
-  imports: [FormularioGeneroComponent],
+  imports: [
+    FormularioGeneroComponent,
+    CargandoComponent,
+    MostrarErroresComponent,
+  ],
   templateUrl: './editar-genero.component.html',
   styleUrl: './editar-genero.component.css',
 })
-export class EditarGeneroComponent {
+export class EditarGeneroComponent implements OnInit {
+  ngOnInit(): void {
+    this.generoService.obtenerPorId(this.id).subscribe((genero) => {
+      this.genero = genero;
+    });
+  }
   @Input({ transform: numberAttribute })
   id!: number;
-
-  genero:GeneroDTO = {id:1,nombre:"Terror"}
+  genero?: GeneroDTO;
+  generoService = inject(GeneroService);
+  errores: string[] = [];
+  router = inject(Router);
 
   guardarCambios(genero: GeneroCreacionDTO) {
-    console.log(`Editando el genero ${genero.nombre}`);
+    this.generoService.actualizar(this.id, genero).subscribe({
+      next: () => {
+        this.router.navigate(['/generos']);
+      },
+      error: (err) => {
+        const errores = extraerErrores(err);
+        this.errores = errores;
+      },
+    });
   }
 }
